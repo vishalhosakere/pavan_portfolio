aea52be1dbdebdd5b12c3942634dd06b226355f5
Vishal Hosakere <mailme.vishalh@gmail.com>
2023-09-26 22:37:14 -0700

    Remove hover on Cards in screen size smaller than lg. Remove arrow since the cards are not links (yet?)

--- src/elements/Card.tsx
+++ src/elements/Card.tsx
@@ -1,5 +1,5 @@
 import Tag from "./Tag";
-import { PiArrowRightBold } from "react-icons/pi";
+// import { PiArrowRightBold } from "react-icons/pi";
 
 type CardProps = {
   year: string;
@@ -12,17 +12,17 @@ type CardProps = {
 const Card = ({ year, title, subtitle, description, tags }: CardProps) => {
   return (
     <li className="mb-12">
-      <div className="group rounded-lg hover:!opacity-100 border-transparent hover:border-accent-800 border-2 group-hover/list:opacity-50 grid sm:grid-cols-10 p-4 transition-colors duration-100">
+      <div className="group rounded-lg hover:!opacity-100 border-transparent lg:hover:border-accent-800 border-2 lg:group-hover/list:opacity-50 grid sm:grid-cols-10 p-4 transition-colors duration-100">
         {year && <div className="sm:col-span-3 mt-1 text-sm">{year}</div>}
         <div
           className={year !== "" ? "sm:col-span-7" : "sm:col-span-10 sm:mx-10"}
         >
-          <div className="text-xl text-primary-200 group-hover:text-accent-200 flex items-center gap-2">
+          <div className="text-xl text-primary-200 lg:group-hover:text-accent-200 flex items-center gap-2">
             {title}
-            <PiArrowRightBold className="group-hover:translate-x-2 duration-200" />
+            {/* <PiArrowRightBold className="lg:group-hover:translate-x-2 duration-200" /> */}
           </div>
           {subtitle != "" && (
-            <div className="text-lg group-hover:text-primary-200">
+            <div className="text-lg lg:group-hover:text-primary-200">
               {subtitle}
             </div>
           )}
