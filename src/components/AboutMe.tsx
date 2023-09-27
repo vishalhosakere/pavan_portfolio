af8336900ad601974a4d085f83aba24c058aac87
Vishal Hosakere <mailme.vishalh@gmail.com>
2023-09-27 00:01:35 -0700

    Add react-highlight-words package Update some other info

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,16 +1,21 @@
 import aboutData from "@/data/aboutme.json";
 
+import Highlighter from "react-highlight-words";
+
 function AboutMe() {
   return (
     <section className="flex flex-col gap-4 pb-24" id="about">
       <h3 className="flex lg:hidden text-primary-200 text-xl uppercase font-bold pb-10">
         About
       </h3>
-      {aboutData.map((para, idx) => {
+      {aboutData.strings.map((text, idx) => {
         return (
-          <p key={idx} className="text-lg">
-            {para}
-          </p>
+          <Highlighter
+            highlightClassName="text-primary-200 bg-transparent"
+            searchWords={aboutData.highlight}
+            autoEscape={true}
+            textToHighlight={text}
+          />
         );
       })}
     </section>
